208bd091ddc5f1838d4bfe25e15243f1dca886c6
Umesh Gainju <umesh.gainju@vedastudios.com>
2025-10-15 10:17:59 +0545

    feat: removing the unncessary logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,6 @@ import type { Request } from 'express'; // Assuming you are using Express as the
 export class AppController {
   @Get('origin')
   getOrigin(@Req() req: Request): string {
-    console.info(req);
-    // const origin = req.headers.host;
     const origin = req.get('Origin'); // Get the 'Origin' header
 
     console.log('Request Origin:', origin);
